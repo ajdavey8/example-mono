b9a85fb4da46cc5a33de493c522719a127204279
Andrew <ajdavey8@gmail.com>
2023-04-19 16:35:00 +0100

    add catalog infos

--- backend/services/golden/jest.config.js
+++
@@ -1,8 +0,0 @@
-const { extendBase } = require("@shieldpay/backend-testing");
-
-module.exports = extendBase({
-  testEnvironment: "node",
-  coveragePathIgnorePatterns: ["/test-helpers.ts"],
-  setupFiles: ["<rootDir>/.jest/env.js"],
-  testEnvironment: "node",
-});
