5b558cb19ab5d3a4e2366239a22b9793fd2e8121
Andrew <ajdavey8@gmail.com>
2023-05-09 20:58:26 +0100

    fix: license and packages

--- packages/backend-common/src/iac/common/resource-name-generator.ts
+++ packages/backend-common/src/iac/common/resource-name-generator.ts
@@ -1,3 +1,6 @@
 export const resourceNameGenerator = (stage: string, prefix?: string) => {
   return (name: string) => `${prefix ? `${prefix}--` : ""}${name}--${stage}`;
 };
+
+const a = true;
+console.log(a);
