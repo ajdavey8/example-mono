6e8f5075e8aa9b91ff4609417b8c720ad7c1842a
Andrew <ajdavey8@gmail.com>
2023-05-16 15:28:21 +0100

    fix: yarnrc

--- backend/services/api-facade/src/index.ts
+++ backend/services/api-facade/src/index.ts
@@ -1,5 +1,3 @@
 import { resourceNameGenerator } from "@andrew-org-test/backend-common";
-import { resourceNameGenerator as test } from "@shieldpay/common";
 
 console.log('Hello from test!', resourceNameGenerator('test'));
-console.log('Hello from test!', test('222'));
