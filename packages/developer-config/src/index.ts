90d8024b68c127b9784ba4e412e2e980db01c274
Andrew <ajdavey8@gmail.com>
2023-08-09 18:07:57 +0100

    feat: add console log to dev con

--- packages/developer-config/src/index.ts
+++ packages/developer-config/src/index.ts
@@ -1,3 +1,5 @@
-export { extendBase } from './jest'
-export { rules as useOptimusEventsPlugin } from './eslint/use-optimus-events'
-export { rules as handlerCaptureExceptionPlugin } from './eslint/handler-capture-exception'
+export { extendBase } from "./jest";
+export { rules as useOptimusEventsPlugin } from "./eslint/use-optimus-events";
+export { rules as handlerCaptureExceptionPlugin } from "./eslint/handler-capture-exception";
+const a = 1;
+console.log(a);
