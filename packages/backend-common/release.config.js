6ddf8094389340197c6f53375058ebe2ab61595c
Andrew <ajdavey8@gmail.com>
2023-08-09 18:18:26 +0100

    chore(backend-common): add release rules

--- packages/backend-common/release.config.js
+++ packages/backend-common/release.config.js
@@ -8,10 +8,10 @@ module.exports = {
       '@semantic-release/commit-analyzer',
       {
         releaseRules: [
-          { type: feat, scope: "backend-common", release: "minor" },
-          { type: feat, release: false },
-          { type: fix, scope: "backend-common", release: "minor" },
-          { type: fix, release: false },
+          { type: "feat", scope: "backend-common", release: "minor" },
+          { type: "feat", release: false },
+          { type: "fix", scope: "backend-common", release: "minor" },
+          { type: "fix", release: false },
           { breaking: true,  scope: "backend-common", release: "major" },
           { breaking: true, release: false },
           { revert: true, scope: "backend-common", release: "patch" },
