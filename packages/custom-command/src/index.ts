db94be4d786c988850f50d85d5e231f546da6c16
Andrew <ajdavey8@gmail.com>
2023-08-20 22:13:04 +0100

    feat: update package

--- packages/custom-command/src/index.ts
+++ packages/custom-command/src/index.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env ts-node
 
-import { exec } from "child_process";
-exec("echo 'Hello World!'");
-exec(`echo 'Hello args' > ${process.argv[2]}`);
+console.log("Hello world");
+console.log(`'Hello args' > ${process.argv[2]}`);
