7b7a72b62211eeafa497529e41bc862fecd041bb
Andrew <ajdavey8@gmail.com>
2023-08-21 16:37:02 +0100

    fix: paths and envs

---
+++ packages/custom-command/src/load-params.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env -S npx ts-node
+
+console.log("Hello params");
+console.log(`'Hello args' > ${process.argv[2]}`);
--- packages/custom-command/src/run-checks.ts
+++ packages/custom-command/src/run-checks.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env ts-node
 
-console.log("Hello world");
+console.log("Hello checks");
 console.log(`'Hello args' > ${process.argv[2]}`);
