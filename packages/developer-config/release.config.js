be561f3b63cf0b3a7685c900a1db291c79d3f976
Andrew <ajdavey8@gmail.com>
2023-08-22 11:34:53 +0100

    feat: trigger

--- packages/developer-config/release.config.js
+++ packages/developer-config/release.config.js
@@ -5,7 +5,21 @@ module.exports = {
   tagFormat: name + '-v${version}',
   branch: 'main',
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { type: "feat", scope: "backend-common", release: "minor" },
+          { type: "feat", release: false },
+          { type: "fix", scope: "backend-common", release: "minor" },
+          { type: "fix", release: false },
+          { breaking: true,  scope: "backend-common", release: "major" },
+          { breaking: true, release: false },
+          { revert: true, scope: "backend-common", release: "patch" },
+          { revert: true, release: false },
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     [
       '@semantic-release/changelog',
